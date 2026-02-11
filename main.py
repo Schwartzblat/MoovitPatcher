56ca9c95e8abc0a2637429384b1d694605826973
alon <alon.ponch@gmail.com>
2026-02-11 22:59:07 +0200

    Remove bin folder and add no sign option

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def get_args():
                         default=None)
     parser.add_argument('--artifactory', dest='artifactory', help='Artifactory path', required=False,
                         default='./artifactory.json')
+    parser.add_argument('--no-sign', dest='should_sign', help='Whether to sign the output APK', action='store_false',
+                        required=False, default=True)
     return parser.parse_args()
 
 
@@ -31,7 +33,8 @@ def main():
             artifactory_list=artifactory_list,
             google_api_key=args.api_key,
             external_modules=[ExternalModule(Path('./smali_generator'),
-                                             'invoke-static {}, Lcom/smali_generator/TheAmazingPatch;->on_load()V')]
+                                             'invoke-static {}, Lcom/smali_generator/TheAmazingPatch;->on_load()V')],
+            should_sign=args.should_sign,
     ) as stitch:
         stitch.patch()
 
